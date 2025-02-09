911017afeb968d0329b81806b826d6086b119c01
Akatronix <153951875+Akatronix@users.noreply.github.com>
2025-02-09 11:43:05 +0100

    Update data.js

--- backend/data.js
+++ backend/data.js
@@ -3,21 +3,21 @@ module.exports = function myData() {
     {
       data: [
         {
-          _id: "6750370e304bdfb8d6692a0f",
+          _id: "679d57d15af155c977dc6fe3",
           current: "0",
           voltage: "0",
           power: "0",
           energy: "0",
         },
         {
-          _id: "6750373c304bdfb8d6692a11",
+          _id: "679d57dc5af155c977dc6fe5",
           current: "0",
           voltage: "0",
           power: "0",
           energy: "0",
         },
         {
-          _id: "6750374b304bdfb8d6692a13",
+          _id: "679d57de5af155c977dc6fe7",
           current: "0",
           voltage: "0",
           power: "0",
@@ -26,14 +26,14 @@ module.exports = function myData() {
           __v: 0,
         },
         {
-          _id: "67503760304bdfb8d6692a15",
+          _id: "679d57e05af155c977dc6fe9",
           current: "0",
           voltage: "0",
           power: "0",
           energy: "0",
         },
         {
-          _id: "67503775304bdfb8d6692a17",
+          _id: "679d57e25af155c977dc6feb",
           current: "0",
           voltage: "0",
           power: "0",
@@ -54,59 +54,3 @@ module.exports = function myData() {
     },
   ];
 };
-
-// {
-//   "data": [
-//      {
-//       "_id": "6750370e304bdfb8d6692a0f",
-//       "current": "10",
-//       "voltage": "210",
-//       "power": "30",
-//       "energy": "45"
-//     },
-//     {
-//       "_id": "6750373c304bdfb8d6692a11",
-//       "current": "100",
-//       "voltage": "220",
-//       "power": "60",
-//       "energy": "0.10"
-//     },
-//     {
-//       "_id": "6750374b304bdfb8d6692a13",
-//       "current": "10",
-//       "voltage": "220",
-//       "power": "6",
-//       "energy": "0.08",
-//       "timestamp": "2024-12-04T11:04:43.996Z",
-//       "__v": 0
-//     },
-//     {
-//       "_id": "67503760304bdfb8d6692a15",
-//       "current": "7",
-//       "voltage": "220",
-//       "power": "500",
-//       "energy": "1.09"
-//     },
-//     {
-//       "_id": "67503775304bdfb8d6692a17",
-//       "current": "12",
-//       "voltage": "220",
-//       "power": "26",
-//       "energy": "56"
-//     }
-//   ],
-//     "info":{
-//       "id": "67508b9695dfb3d2cb799ea5",
-//        "mydata":{
-//           "socketone":"ON",
-//           "sockettwo":"OFF",
-//           "socketthree":"OFF",
-//           "socketfour":"ON",
-//           "socketfive":"ON"
-//        }
-//   },
-//   "total":{
-//     "tid":"679d58af5af155c977dc7008",
-//      "totalEnergy":"59090"
-//   }
-// }
